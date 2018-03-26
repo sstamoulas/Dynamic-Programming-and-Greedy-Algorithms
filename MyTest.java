ac9d6f13ee5ace68d67678f55aec0062d6ca9689
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2018-03-26 15:23:16 +0300

    Create MyTest.java

---
+++ MyTest.java
@@ -0,0 +1,37 @@
+package dynamicProgramming;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.ArrayList;
+
+public class MyTest {
+	
+	/*TODO: Add your own test cases here!
+	 * We've provided a sample test case for each problem below
+	 * You can use these as building blocks to write your own test cases
+	 */
+	
+	@Test
+	public void HuffmanSampleTest() {
+		String input = "abc";
+		Huffman h = new Huffman(input);
+		String encoding = h.encode();
+		assertEquals(input, h.decode(encoding));
+		assertEquals("huffman abc compression", Huffman.compressionRatio(input), 0.20833, 0.01);
+	}
+	
+	@Test
+	public void IntervalSampleTest() {
+		GreedyDynamicAlgorithms.Interval red = new GreedyDynamicAlgorithms.Interval(1, 3);
+		GreedyDynamicAlgorithms.Interval blue = new GreedyDynamicAlgorithms.Interval(0, 4);
+		ArrayList<GreedyDynamicAlgorithms.Interval> reds = new ArrayList<>();
+		ArrayList<GreedyDynamicAlgorithms.Interval> blues = new ArrayList<>();
+		reds.add(red);
+		blues.add(blue);
+		int expectedOptimal = 1;
+		int actualOptimal = GreedyDynamicAlgorithms.optimalIntervals(reds, blues);
+		assertEquals("interval 1 red 1 blue", expectedOptimal, actualOptimal);
+	}
+
+}
+
