d77ed6f3c3e7ef07ff46d84f2e9c05d915d78cd3
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2018-04-09 09:12:49 +0300

    Added Tests for Optimal Interval Problem

--- MyTest.java
+++ MyTest.java
@@ -21,7 +21,7 @@ public class MyTest {
 	}
 	
 	@Test
-	public void IntervalSampleTest() {
+	public void IntervalSampleTest1() {
 		GreedyDynamicAlgorithms.Interval red = new GreedyDynamicAlgorithms.Interval(1, 3);
 		GreedyDynamicAlgorithms.Interval blue = new GreedyDynamicAlgorithms.Interval(0, 4);
 		ArrayList<GreedyDynamicAlgorithms.Interval> reds = new ArrayList<>();
@@ -33,5 +33,120 @@ public class MyTest {
 		assertEquals("interval 1 red 1 blue", expectedOptimal, actualOptimal);
 	}
 
+	@Test
+	public void IntervalSampleTest2() {
+		GreedyDynamicAlgorithms.Interval red1 = new GreedyDynamicAlgorithms.Interval(0, 4);
+		GreedyDynamicAlgorithms.Interval red2 = new GreedyDynamicAlgorithms.Interval(2, 5);
+		GreedyDynamicAlgorithms.Interval red3 = new GreedyDynamicAlgorithms.Interval(4, 8);
+		GreedyDynamicAlgorithms.Interval red4 = new GreedyDynamicAlgorithms.Interval(9, 10);
+		GreedyDynamicAlgorithms.Interval red5 = new GreedyDynamicAlgorithms.Interval(9, 11);
+		GreedyDynamicAlgorithms.Interval red6 = new GreedyDynamicAlgorithms.Interval(10, 12);
+		GreedyDynamicAlgorithms.Interval red7 = new GreedyDynamicAlgorithms.Interval(11, 12);
+		GreedyDynamicAlgorithms.Interval blue1 = new GreedyDynamicAlgorithms.Interval(0, 2);
+		GreedyDynamicAlgorithms.Interval blue2 = new GreedyDynamicAlgorithms.Interval(5, 5);
+		GreedyDynamicAlgorithms.Interval blue3 = new GreedyDynamicAlgorithms.Interval(7, 10);
+		GreedyDynamicAlgorithms.Interval blue4 = new GreedyDynamicAlgorithms.Interval(11, 13);
+		ArrayList<GreedyDynamicAlgorithms.Interval> reds = new ArrayList<>();
+		ArrayList<GreedyDynamicAlgorithms.Interval> blues = new ArrayList<>();
+		reds.add(red1);
+		reds.add(red2);
+		reds.add(red3);
+		reds.add(red4);
+		reds.add(red5);
+		reds.add(red6);
+		reds.add(red7);
+		blues.add(blue1);
+		blues.add(blue2);
+		blues.add(blue3);
+		blues.add(blue4);
+		int expectedOptimal = 2;
+		int actualOptimal = GreedyDynamicAlgorithms.optimalIntervals(reds, blues);
+		assertEquals("interval 1 red 1 blue", expectedOptimal, actualOptimal);
+	}
+	
+	@Test
+	public void IntervalSampleTest3() {
+		GreedyDynamicAlgorithms.Interval red3 = new GreedyDynamicAlgorithms.Interval(4, 8);
+		GreedyDynamicAlgorithms.Interval red4 = new GreedyDynamicAlgorithms.Interval(9, 10);
+		GreedyDynamicAlgorithms.Interval red5 = new GreedyDynamicAlgorithms.Interval(9, 11);
+		GreedyDynamicAlgorithms.Interval red6 = new GreedyDynamicAlgorithms.Interval(10, 12);
+		GreedyDynamicAlgorithms.Interval red7 = new GreedyDynamicAlgorithms.Interval(11, 12);
+		GreedyDynamicAlgorithms.Interval blue1 = new GreedyDynamicAlgorithms.Interval(0, 2);
+		GreedyDynamicAlgorithms.Interval blue2 = new GreedyDynamicAlgorithms.Interval(5, 5);
+		GreedyDynamicAlgorithms.Interval blue3 = new GreedyDynamicAlgorithms.Interval(7, 10);
+		GreedyDynamicAlgorithms.Interval blue4 = new GreedyDynamicAlgorithms.Interval(11, 13);
+		ArrayList<GreedyDynamicAlgorithms.Interval> reds = new ArrayList<>();
+		ArrayList<GreedyDynamicAlgorithms.Interval> blues = new ArrayList<>();
+		reds.add(red3);
+		reds.add(red4);
+		reds.add(red5);
+		reds.add(red6);
+		reds.add(red7);
+		blues.add(blue1);
+		blues.add(blue2);
+		blues.add(blue3);
+		blues.add(blue4);
+		int expectedOptimal = -1;
+		int actualOptimal = GreedyDynamicAlgorithms.optimalIntervals(reds, blues);
+		assertEquals("interval 1 red 1 blue", expectedOptimal, actualOptimal);
+	}
+	
+	@Test
+	public void IntervalSampleTest4() {
+		GreedyDynamicAlgorithms.Interval red1 = new GreedyDynamicAlgorithms.Interval(0, 10);
+		GreedyDynamicAlgorithms.Interval red2 = new GreedyDynamicAlgorithms.Interval(2, 5);
+		GreedyDynamicAlgorithms.Interval red3 = new GreedyDynamicAlgorithms.Interval(4, 8);
+		GreedyDynamicAlgorithms.Interval red4 = new GreedyDynamicAlgorithms.Interval(9, 10);
+		GreedyDynamicAlgorithms.Interval red5 = new GreedyDynamicAlgorithms.Interval(9, 11);
+		GreedyDynamicAlgorithms.Interval red6 = new GreedyDynamicAlgorithms.Interval(10, 12);
+		GreedyDynamicAlgorithms.Interval red7 = new GreedyDynamicAlgorithms.Interval(11, 12);
+		GreedyDynamicAlgorithms.Interval blue1 = new GreedyDynamicAlgorithms.Interval(0, 2);
+		GreedyDynamicAlgorithms.Interval blue2 = new GreedyDynamicAlgorithms.Interval(5, 5);
+		GreedyDynamicAlgorithms.Interval blue3 = new GreedyDynamicAlgorithms.Interval(7, 10);
+		GreedyDynamicAlgorithms.Interval blue4 = new GreedyDynamicAlgorithms.Interval(11, 13);
+		ArrayList<GreedyDynamicAlgorithms.Interval> reds = new ArrayList<>();
+		ArrayList<GreedyDynamicAlgorithms.Interval> blues = new ArrayList<>();
+		reds.add(red1);
+		reds.add(red2);
+		reds.add(red3);
+		reds.add(red4);
+		reds.add(red5);
+		reds.add(red6);
+		reds.add(red7);
+		blues.add(blue1);
+		blues.add(blue2);
+		blues.add(blue3);
+		blues.add(blue4);
+		int expectedOptimal = 2;
+		int actualOptimal = GreedyDynamicAlgorithms.optimalIntervals(reds, blues);
+		assertEquals("interval 1 red 1 blue", expectedOptimal, actualOptimal);
+	}
+	
+	@Test
+	public void IntervalSampleTest5() {
+		GreedyDynamicAlgorithms.Interval red1 = new GreedyDynamicAlgorithms.Interval(0, 10);
+		GreedyDynamicAlgorithms.Interval red2 = new GreedyDynamicAlgorithms.Interval(2, 5);
+		GreedyDynamicAlgorithms.Interval red3 = new GreedyDynamicAlgorithms.Interval(4, 8);
+		GreedyDynamicAlgorithms.Interval red4 = new GreedyDynamicAlgorithms.Interval(9, 10);
+		GreedyDynamicAlgorithms.Interval red5 = new GreedyDynamicAlgorithms.Interval(9, 11);
+		GreedyDynamicAlgorithms.Interval blue1 = new GreedyDynamicAlgorithms.Interval(0, 2);
+		GreedyDynamicAlgorithms.Interval blue2 = new GreedyDynamicAlgorithms.Interval(5, 5);
+		GreedyDynamicAlgorithms.Interval blue3 = new GreedyDynamicAlgorithms.Interval(7, 10);
+		GreedyDynamicAlgorithms.Interval blue4 = new GreedyDynamicAlgorithms.Interval(11, 13);
+		ArrayList<GreedyDynamicAlgorithms.Interval> reds = new ArrayList<>();
+		ArrayList<GreedyDynamicAlgorithms.Interval> blues = new ArrayList<>();
+		reds.add(red1);
+		reds.add(red2);
+		reds.add(red3);
+		reds.add(red4);
+		reds.add(red5);
+		blues.add(blue1);
+		blues.add(blue2);
+		blues.add(blue3);
+		blues.add(blue4);
+		int expectedOptimal = 2;
+		int actualOptimal = GreedyDynamicAlgorithms.optimalIntervals(reds, blues);
+		assertEquals("interval 1 red 1 blue", expectedOptimal, actualOptimal);
+	}
 }
 
